70100c8c190ed6bf584eb2aba2b8f404d62c3867
Hema Venkata <hvenkata@live.com>
2019-06-25 01:54:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,13 +59,13 @@ function removeFromCart(item) {
     }
 
 function placeOrder(cardNumber) {
-//  var emptyArray = [];
-  if (cardNumber === undefined) {
-    return "Sorry, we don't have a credit card on file for you."
-  } else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-    setCart([]);
-    //cart.splice(0, cart.length)
+    var freshArray = [];
+    if (cardNumber === undefined) {
+      return "Sorry, we don't have a credit card on file for you."
+    } else {
+      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    setCart(freshArray)
+  //  cart.splice(0, cart.length)
   }
   //return cart
 }
